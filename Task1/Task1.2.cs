11b53f39af48b152c7fa6e204cde2479fd913675
llllenah <105115169+llllenah@users.noreply.github.com>
2022-11-21 22:44:40 +0200

    Create Task1.2.cs

---
+++ Task1/Task1.2.cs
@@ -0,0 +1,35 @@
+using System;
+
+public class Program
+{
+    static bool IsPrime(int number){
+       if (number <= 1){
+           return false;
+       }
+       for (int i = 2; i * i <= number; i++){
+           if (number % i == 0){
+               return false;
+           }
+       }
+       return true;
+    }
+    static int theNearestPrime(int number){
+        for (int i = 1; i < number; i++){
+            if (IsPrime(number+i)){
+                return number + i;
+            }
+            else if(IsPrime(number-i)){
+                return number - i;
+            }
+        }
+        return 1;
+    }
+    public static void Main()
+    {
+        Console.WriteLine("Введіть число");
+        int number = int.Parse(Console.ReadLine());
+        
+        int theClosestPrime = theNearestPrime(number);
+        Console.WriteLine($"Найближче просте число = {theClosestPrime}");
+    }
+}
